c152f57afd4f96c6fd556b32d5812d5a354db746
Guerric Sloan <guerric.sloan@gmail.com>
2014-11-30 20:53:03 -0800

    Remove hard dependency on require-js and add support for node

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 jaqb
 ====
+> JavaScript Query Builder for SQLite
+
+Check out the specs for usage information.
+
+## Running tests
+Start the simple HTTP server. Navigate to [localhost:8080](http://localhost:8080/SpecRunner.html)
+```shell
+npm start
+```
--- src/jaqb.js
+++ src/jaqb.js
@@ -5,216 +5,229 @@
  * @repository https://github.com/fdn/jaqb
  * @license Licensed under MIT
  */
-define(function(require) {
-  var _ = require('underscore');
+(function() {
 
-  var TYPE_SELECT = 'SELECT';
-  var TYPE_INSERT = 'INSERT';
+  function injectDependencies(_) {
+    var TYPE_SELECT = 'SELECT';
+    var TYPE_INSERT = 'INSERT';
 
-  function Query(type, tables) {
+    function Query(type, tables) {
 
-    // select, insert
-    this._type = type;
-    this._tables = tables || [];
-    if (this._type == TYPE_INSERT) {
-      this._fields = new InsertFields();
-    } else {
-      this._fields = new QueryFields();
-    }
-  }
-
-  _.extend(Query.prototype, {
-
-    field: function(key, value) {
-      if (arguments.length == 0) {
-        throw 'No field name specified';
-      }
-      if (this._type == TYPE_SELECT) {
-        _.each(arguments, function(arg) {
-          this._fields.set(arg);
-        }, this);
+      // select, insert
+      this._type = type;
+      this._tables = tables || [];
+      if (this._type == TYPE_INSERT) {
+        this._fields = new InsertFields();
       } else {
-        this._fields.set(key, value);
+        this._fields = new QueryFields();
       }
-      return this;
-    },
-
-    tables: function(/* table1, ... */) {
-      this._tables = this._tables.concat(_.toArray(arguments));
-      return this;
-    },
-
-    where: function(/* field1, op, field2? */) {
-      var args = _.toArray(arguments);
-
-      // query condition builder mode!
-      if (args.length == 0) {
-        var condition = new ConditionBuilder(this);
-        this._where = condition;
-        return condition;
-      }
-
-      // @note: saving for future use
-      throw 'Not supported';
-    },
-
-    /**
-     * Processes the pieces to build the query
-     */
-    toString: function() {
-      var s;
-      switch (this._type) {
-        case TYPE_SELECT:
-          s = this._type + ' ' + this._fields.toString() + ' FROM ' + this._tables.join(', ');
-          if (this._where) {
-            var where = this._where.toString();
-            if (where) {
-              s += ' WHERE ' + where;
-            }
-          }
-          break;
-
-        case TYPE_INSERT:
-          s = this._type + ' INTO ' + this._tables[0] + ' ' + this._fields.toString();
-          break;
-      }
-      return s;
     }
-  });
 
-  function InsertFields(fields) {
+    _.extend(Query.prototype, {
 
-    // TODO: ensure incoming fields is an object
-    this._fields = fields || {};
-  }
+      field: function (key, value) {
+        if (arguments.length == 0) {
+          throw 'No field name specified';
+        }
+        if (this._type == TYPE_SELECT) {
+          _.each(arguments, function (arg) {
+            this._fields.set(arg);
+          }, this);
+        } else {
+          this._fields.set(key, value);
+        }
+        return this;
+      },
 
-  _.extend(InsertFields.prototype, {
-    set: function(field, value) {
-      this._fields[field] = value;
-    },
+      tables: function (/* table1, ... */) {
+        this._tables = this._tables.concat(_.toArray(arguments));
+        return this;
+      },
 
-    toString: function() {
-      var keys = _.keys(this._fields);
-      var values = _.values(this._fields);
-      values = _.map(values, function(value) {
+      where: function (/* field1, op, field2? */) {
+        var args = _.toArray(arguments);
+
+        // query condition builder mode!
+        if (args.length == 0) {
+          var condition = new ConditionBuilder(this);
+          this._where = condition;
+          return condition;
+        }
+
+        // @note: saving for future use
+        throw 'Not supported';
+      },
+
+      /**
+       * Processes the pieces to build the query
+       */
+      toString: function () {
+        var s;
+        switch (this._type) {
+          case TYPE_SELECT:
+            s = this._type + ' ' + this._fields.toString() + ' FROM ' + this._tables.join(', ');
+            if (this._where) {
+              var where = this._where.toString();
+              if (where) {
+                s += ' WHERE ' + where;
+              }
+            }
+            break;
+
+          case TYPE_INSERT:
+            s = this._type + ' INTO ' + this._tables[0] + ' ' + this._fields.toString();
+            break;
+        }
+        return s;
+      }
+    });
+
+    function InsertFields(fields) {
+
+      // TODO: ensure incoming fields is an object
+      this._fields = fields || {};
+    }
+
+    _.extend(InsertFields.prototype, {
+      set: function (field, value) {
+        this._fields[field] = value;
+      },
+
+      toString: function () {
+        var keys = _.keys(this._fields);
+        var values = _.values(this._fields);
+        values = _.map(values, function (value) {
+
+          // TODO: escape the value
+          return "'" + value + "'";
+        });
+        return '(' + keys.join(', ') + ') VALUES (' + values.join(', ') + ')';
+      }
+    });
+
+    function QueryFields(fields) {
+      this._fields = fields || [];
+    }
+
+    _.extend(QueryFields.prototype, {
+
+      // TODO: dedupe set fields
+      set: function (field, value) {
+        this._fields.push(field);
+        return this;
+      },
+
+      toString: function () {
+        if (this._fields.length == 0) {
+          return '*';
+        }
+        var fields = this._fields.join(', ');
+        return fields;
+      }
+    });
+
+    function ConditionBuilder(query) {
+      this._parentQuery = query;
+      this._type = 'where';
+      this._field = 1;
+
+      this._nextCondition = null;
+    }
+
+    _.extend(ConditionBuilder.prototype, {
+      field: function (name) {
+        this['_field' + this._field] = name;
+        this._field++;
+        return this;
+      },
+
+      equals: function () {
+        this._operator = '=';
+        return this;
+      },
+
+      gt: function () {
+        this._operator = '>';
+        return this;
+      },
+
+      lt: function () {
+        this._operator = '<';
+        return this;
+      },
+
+      lte: function () {
+        this._operator = '<=';
+        return this;
+      },
+
+      gte: function () {
+        this._operator = '>=';
+        return this;
+      },
+
+      value: function (value) {
 
         // TODO: escape the value
-        return "'" + value + "'";
-      });
-      return '('+ keys.join(', ') + ') VALUES ('+ values.join(', ') +')';
-    }
-  });
+        this['_field' + this._field] = "'" + value + "'";
+        return this;
+      },
 
-  function QueryFields(fields) {
-    this._fields = fields || [];
-  }
+      and: function () {
+        this._nextOperator = 'AND';
+        this._nextCondition = new ConditionBuilder(this._parentQuery);
+        return this._nextCondition;
+      },
 
-  _.extend(QueryFields.prototype, {
+      or: function () {
+        this._nextOperator = 'OR';
+        this._nextCondition = new ConditionBuilder(this._parentQuery);
+        return this._nextCondition;
+      },
 
-    // TODO: dedupe set fields
-    set: function(field, value) {
-      this._fields.push(field);
-      return this;
-    },
+      /**
+       * Chaining helper
+       * @return {Query}
+       */
+      end: function () {
+        return this._parentQuery;
+      },
 
-    toString: function() {
-      if (this._fields.length == 0) {
-        return '*';
+      toString: function () {
+        var condition = this._field1 + ' ' + this._operator + ' ' + this._field2;
+        if (this._nextCondition) {
+          condition += ' ' + this._nextOperator + ' (' + this._nextCondition.toString() + ')';
+        }
+        return condition;
       }
-      var fields = this._fields.join(', ');
-      return fields;
+    });
+
+    function select(/* tables */) {
+      return new Query(TYPE_SELECT, _.toArray(arguments));
     }
-  });
 
-  function ConditionBuilder(query) {
-    this._parentQuery = query;
-    this._type = 'where';
-    this._field = 1;
-
-    this._nextCondition = null;
-  }
-
-  _.extend(ConditionBuilder.prototype, {
-    field: function(name) {
-      this['_field' + this._field] = name;
-      this._field++;
-      return this;
-    },
-
-    equals: function() {
-      this._operator = '=';
-      return this;
-    },
-
-    gt: function() {
-      this._operator = '>';
-      return this;
-    },
-
-    lt: function() {
-      this._operator = '<';
-      return this;
-    },
-
-    lte: function() {
-      this._operator = '<=';
-      return this;
-    },
-
-    gte: function() {
-      this._operator = '>=';
-      return this;
-    },
-
-    value: function(value) {
-
-      // TODO: escape the value
-      this['_field' + this._field] = "'" + value + "'";
-      return this;
-    },
-
-    and: function() {
-      this._nextOperator = 'AND';
-      this._nextCondition = new ConditionBuilder(this._parentQuery);
-      return this._nextCondition;
-    },
-
-    or: function() {
-      this._nextOperator = 'OR';
-      this._nextCondition = new ConditionBuilder(this._parentQuery);
-      return this._nextCondition;
-    },
-
-    /**
-     * Chaining helper
-     * @return {Query}
-     */
-    end: function() {
-      return this._parentQuery;
-    },
-
-    toString: function() {
-      var condition = this._field1 + ' ' + this._operator + ' ' + this._field2;
-      if (this._nextCondition) {
-        condition += ' ' + this._nextOperator + ' (' + this._nextCondition.toString() + ')';
+    function insert(/* table */) {
+      if (arguments.length == 0) {
+        throw 'Missing insert table';
       }
-      return condition;
+      return new Query(TYPE_INSERT, _.toArray(arguments));
     }
-  });
 
-  function select(/* tables */) {
-    return new Query(TYPE_SELECT, _.toArray(arguments));
+    return {
+      select: select,
+      insert: insert
+    }
   }
 
-  function insert(/* table */) {
-    if (arguments.length == 0) {
-      throw 'Missing insert table';
-    }
-    return new Query(TYPE_INSERT, _.toArray(arguments));
+  // export
+  if (typeof define !== 'undefined') {
+    define(function(require) {
+      var _ = require('underscore');
+      return injectDependencies(_);
+    });
   }
-
-  return {
-    select: select,
-    insert: insert
-  };
-});
+  if (typeof module !== 'undefined') {
+    var _ = require('underscore');
+    module.exports = injectDependencies(_);
+  }
+})();
